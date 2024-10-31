064fca3cbdb1957efb969db3c3246fa2fd8d4393
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2024-10-31 10:49:15 +0100

    black formatting

--- logger.py
+++ logger.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 file_handler = RotatingFileHandler(LOG_FILE, maxBytes=5 * 1024 * 1024, backupCount=5)
 file_handler.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
